49d654255950e8699da2b391a2b43d88108ac057
Thiago Ramos <thiago@remote.com>
2022-07-16 22:48:32 -0300

    Add seed data for courses and added images for the courses in the seed.

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -14,3 +14,27 @@ Code.require_file("test/support/fixtures/accounts_fixtures.ex")
 
 # Add a user
 Athena.AccountsFixtures.user_fixture(%{email: "test@test.com", password: "123123123123123"})
+
+Athena.Catalog.create_course(%{
+  description: "This course will teach you how to be a better ios developer",
+  cover_image_url: "/images/ios.jpg",
+  name: "iOS for the Future",
+  labels: "iOS, mobile",
+  price: 9999
+})
+
+Athena.Catalog.create_course(%{
+  description: "This course will let you understand the basics of elixir",
+  cover_image_url: "/images/elixir.jpg",
+  name: "Learn Elixir with Me",
+  labels: "elixir, language",
+  price: 9999
+})
+
+Athena.Catalog.create_course(%{
+  description: "This course will make you the best with Phoenix Framework",
+  cover_image_url: "/images/phoenix-course.jpg",
+  name: "Phoenix Framework from zero to hero",
+  labels: "elixir, language",
+  price: 9999
+})
